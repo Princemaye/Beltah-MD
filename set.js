ccf7ef1b9a183fa8f764b02fe529066ae720d986
Prince mayel <145528426+Princemaye@users.noreply.github.com>
2024-07-14 07:03:09 +0100

    Update set.js

--- set.js
+++ set.js
@@ -7,15 +7,15 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || 'zokk',
+module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "Beltah Tech",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "Beltah KE",              
+    OWNER_NAME: process.env.OWNER_NAME || "PRINCE",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "237682698517",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
-    BOT : process.env.BOT_NAME || 'BELTAH_MD',
+    BOT : process.env.BOT_NAME || 'FARADAY',
     URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg',
-    MODE: process.env.PUBLIC_MODE || "yes",
+    MODE: process.env.PUBLIC_MODE || "no",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
     HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
